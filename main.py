7eead3eff1dd8834e0b9dc3a2900c677edc046be
shenoisz <kamikazi3200>
2018-05-07 17:56:08 -0300

    downloadable

--- main.py
+++ main.py
@@ -3,29 +3,46 @@
 
 __author__ = "SHENOISZ"
 
-import requests
+import requests, os
 
 lista = []
 prefix = 'https://storage.googleapis.com/files-download/files_backup/apks/'
-to = '/home/ziggi/files/apks/'
-#to = '/home/shenoisz/Documents/estudos/python/load-files/files/'
+#to = '/home/ziggi/files/apks/'
+to = '/home/shenoisz/Documents/estudos/python/load-files/files/'
 
+log = os.path.abspath(__file__)
+log = str(log).replace(__file__, '') + 'apks.log'
+
+print log
 
 with open('apks_storage.txt', 'r') as f:
     lista = f.read().split(';')
     f.close()
 
 def main():
+
+    count = 0
+    errors = 0
+
     for l in lista:
         try:
             response = requests.get(l)
-            name = str(l).replace(prefix, '')
+            name = l.replace(prefix, '')
             with open(to + name, 'wb') as f:
                 f.write(response.content)
                 f.close()
                 print(name)
-        except:
-            pass    
+                count += 1
+                with open(log, 'a+') as a:
+                    a.write('Count: ' + str(count) + ', ' + l + '\n')
+                    a.close()   
+        except Exception as e:
+            print(e)
+            errors += 1
+
+    with open(log, 'a+') as f:
+        f.write('Total: ' + str(count + errors) + ', Count: ' + str(count) + ', Errors: ' + str(errors))
+        f.close()           
 
 if __name__ == '__main__':
     main()
